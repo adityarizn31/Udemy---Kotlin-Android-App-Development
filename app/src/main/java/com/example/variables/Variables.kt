32532200054cb4ab0457ef919e910ea2c21ec960
[adityarn] <[adityarizkiawann@gmail.com]>
2024-11-30 05:39:13 +0700

    Learning 2

---
+++ app/src/main/java/com/example/variables/Variables.kt
@@ -0,0 +1,20 @@
+package com.example.variables
+
+fun main (args : Array<String>) {
+    var a = 10
+    var b = 20
+    var c = a + b
+
+    print(c)
+
+    var age = 100 // valid
+    var _age = 20 // valid
+    var age3 = 200 // valid
+
+//    var a ge = 10 // invalid
+//    var 3age = 10 // invalid
+
+    var u = 20
+    u = 40
+    print(u)
+}
