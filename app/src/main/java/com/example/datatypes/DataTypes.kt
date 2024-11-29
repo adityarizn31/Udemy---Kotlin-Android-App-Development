663874bcc6813e89f4545b24d7319d14f17c8b21
[adityarn] <[adityarizkiawann@gmail.com]>
2024-11-30 05:41:00 +0700

    Learning 3

---
+++ app/src/main/java/com/example/datatypes/DataTypes.kt
@@ -0,0 +1,21 @@
+package com.example.datatypes
+
+fun main(args : Array <String>) {
+    var a : Boolean = true
+    var b : Char = 'S'
+    var c : Byte = 12
+    var d : Short = -356
+    var e : Int = 43434
+    var f : Long = 1212L
+    var g : Float = 5.1123131F
+    var h : Double = 7.33434
+
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+    print(f)
+    print(g)
+    print(h)
+}
