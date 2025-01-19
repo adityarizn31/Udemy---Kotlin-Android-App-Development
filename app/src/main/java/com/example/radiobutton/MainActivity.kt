c766fd73bb6f78d91babbf252281b8af87d02b6f
[adityarn] <[adityarizkiawann@gmail.com]>
2025-01-20 02:12:22 +0700

    Learn Radio Button in Android

---
+++ app/src/main/java/com/example/radiobutton/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.radiobutton
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RadioButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var linearLayout: LinearLayout
+    lateinit var btnoke : Button
+    lateinit var greenRB : RadioButton
+    lateinit var yellowRB : RadioButton
+    lateinit var redRB : RadioButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        linearLayout = findViewById(R.id.main)
+        btnoke = findViewById(R.id.okBtn)
+        greenRB = findViewById(R.id.greenRB)
+        yellowRB = findViewById(R.id.yellowRB)
+        redRB = findViewById(R.id.redRB)
+
+        btnoke.setOnClickListener {
+            if(greenRB.isChecked) {
+                linearLayout.setBackgroundColor(Color.GREEN)
+            } else if(yellowRB.isChecked) {
+                linearLayout.setBackgroundColor(Color.YELLOW)
+            } else if (redRB.isChecked) {
+                linearLayout.setBackgroundColor(Color.RED)
+            }
+        }
+
+    }
+}
