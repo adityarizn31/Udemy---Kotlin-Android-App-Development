528e17ef470dba7fdd4661fc151c792cb67ad490
[adityarn] <[adityarizkiawann@gmail.com]>
2025-01-23 15:24:29 +0700

    Learning SnackBar Message

--- app/src/main/java/com/example/toastmessage/MainActivity.kt
+++ app/src/main/java/com/example/toastmessage/MainActivity.kt
@@ -5,12 +5,17 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnToast : Button
+
+    lateinit var btnSnackBar : Button
+    lateinit var myLayout : ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +29,15 @@ class MainActivity : AppCompatActivity() {
 
         btnToast = findViewById(R.id.btnToast)
 
+        btnSnackBar = findViewById(R.id.btnSnackBar)
+        myLayout = findViewById(R.id.main)
+
         btnToast.setOnClickListener {
             Toast.makeText(applicationContext, "Button Toast is Clicked",Toast.LENGTH_LONG).show()
+        }
+
+        btnSnackBar.setOnClickListener {
+            Snackbar.make(myLayout, "SnackBar is CLicked", Toast.LENGTH_LONG).show()
         }
     }
 }
